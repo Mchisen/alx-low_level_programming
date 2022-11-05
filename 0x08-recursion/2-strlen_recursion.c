a665f52c952c1592f6deccc4be5dfe580212959b
Mchisen <chisengamwewa1996@gmail.com>
2022-11-05 09:19:02 -0700

    Task 2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -8,11 +8,7 @@
 
 int _strlen_recursion(char *s)
 {
-	int len = 0;
-
-	if (*s)
-	{
-		len++;
-		len = _strlen_recursion(s + 1);
-	}
+	if (!*s)
+		return (0);
+	return (1 + _strlen_recursion(++s));
 }
